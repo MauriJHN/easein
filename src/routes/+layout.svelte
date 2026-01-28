26bac116041b0dca2b896240f290d55336d997a4
Mauricio Hernandez <maurijhn1@gmail.com>
2026-01-28 15:11:58 -0400

    Removed paraglide reference inside +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,7 @@
 <script lang="ts">
-	import { page } from '$app/state';
-	import { locales, localizeHref } from '../../src-archive/paraglide/runtime';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
 
 {@render children()}
-<div style="display:none">
-	{#each locales as locale}
-		<a href={localizeHref(page.url.pathname, { locale })}>
-			{locale}
-		</a>
-	{/each}
-</div>
